e251311c0051d2b41ac33486c48da221fa32cfc8
alexander1914 <alexander.oliveira99@gmail.com>
2025-03-31 19:36:33 +0100

    [Create new service] - I had been learning more firbase crud conceps

--- angularfireapp/src/app/create-course/create-course.component.ts
+++ angularfireapp/src/app/create-course/create-course.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 import { AngularFireStorage } from '@angular/fire/storage';
 import firebase from 'firebase/app';
 import Timestamp = firebase.firestore.Timestamp;
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'create-course',
@@ -16,32 +17,62 @@ import Timestamp = firebase.firestore.Timestamp;
 })
 export class CreateCourseComponent implements OnInit {
 
+  courseId: string;
+
   //To create Validators your form
   form = this.fb.group({
     description: ['', Validators.required],
-    category: ['BEGINNER', Validators.required],
+    categories: ["BEGINNER", Validators.required],
     url: ['', Validators.required],
     longDescription: ['', Validators.required],
     promo: [false],
     promoStartAt: [null]
   })
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private coursesService: CoursesService,
+    private afs: AngularFirestore,
+    private router: Router) {
 
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    // this method is AngularFirestore to create ID your entity
+    this.courseId = this.afs.createId();
+  }
 
   onCreateCourse() {
 
-    const newCourse = { ...this.form.value } as Course;
+    const formValue = this.form.value;
+
+    //Binding of form to Entity on firebasestore
+    const newCourse: Partial<Course> = {
+      description: formValue.description,
+      url: formValue.url,
+      longDescription: formValue.longDescription,
+      promo: formValue.promo,
+      categories: formValue.categories
+    }
 
     //a way to convert a Data with lib firebase
     newCourse.promoStartAt = Timestamp.fromDate(this.form.value.promoStartAt);
 
     console.log(newCourse);
 
+    this.coursesService.createCourse(newCourse, this.courseId)
+      .pipe(
+        tap(course => {
+          console.log("Created new course: ", course);
+          this.router.navigateByUrl("/courses");
+        }),
+        catchError(err => {
+          console.log(err);
+          alert("Couldn't create new course.");
+          return throwError(err);
+        })
 
+      )
+      .subscribe();
   }
 
 }
--- angularfireapp/src/app/services/courses.service.ts
+++ angularfireapp/src/app/services/courses.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Course } from '../model/course';
-import { map } from 'rxjs/operators';
+import { concatMap, map } from 'rxjs/operators';
 import { convertSnaps } from './utils';
 
 @Injectable({
@@ -11,6 +11,47 @@ import { convertSnaps } from './utils';
 export class CoursesService {
 
   constructor(private db: AngularFirestore) { }
+
+  createCourse(newCourse: Partial<Course>, courseId?: string) {
+    return this.db.collection("courses",
+      ref => ref.orderBy("seqNo", "desc").limit(1))
+      .get()
+      .pipe(
+        concatMap(result => {
+
+          const courses = convertSnaps<Course>(result);
+
+          const lastCourseSeqNo = courses[0]?.seqNo ?? 0;
+
+          const course = {
+            ...newCourse,
+            seqNo: lastCourseSeqNo + 1
+          }
+
+          let save$: Observable<any>;
+
+          if (courseId) {
+            //To implement a logic to save new entity on firebasestore by ID
+            save$ = from(this.db.doc(`courses/${courseId}`).set(course));
+          } else {
+            //To implement a logic to save new entity on firebasestore automatily
+            save$ = from(this.db.collection("courses").add(course));
+          }
+
+          return save$
+            .pipe(
+              map(res => {
+                return {
+                  id: courseId ?? res.id,
+                  ...course
+                }
+              })
+
+            );
+
+        })
+      )
+  }
 
   loadCoursesByCategory(category: string): Observable<Course[]> {
 
